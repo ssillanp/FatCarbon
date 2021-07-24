6acd94093da85bbbe62ea49dc0cb27d19f5ed98f
sami <sami.sillanpaa@iki.fi>
2021-07-24 09:02:07 +0300

    Added strings and user validation

--- app/src/main/java/com/example/fatcarbon/MainActivityLoggedIn.java
+++ app/src/main/java/com/example/fatcarbon/MainActivityLoggedIn.java
@@ -18,6 +18,7 @@ public class MainActivityLoggedIn extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
