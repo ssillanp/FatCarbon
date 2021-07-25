1b5a39d0a697091eb646c585c4885f21dcc11c56
sami <sami.sillanpaa@iki.fi>
2021-07-25 22:34:50 +0300

    Food view list ok

---
+++ app/src/main/java/com/example/fatcarbon/TodaysFoodListAdapter.java
@@ -0,0 +1,62 @@
+package com.example.fatcarbon;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class TodaysFoodListAdapter extends RecyclerView.Adapter<TodaysFoodListAdapter.MyViewHolder>{
+
+    ArrayList<DiaryItem> itemsList;
+    Context context;
+    User user;
+
+    public TodaysFoodListAdapter(FragmentActivity ct, ArrayList<DiaryItem> items, User usr){
+        itemsList = items;
+        context = ct;
+        user = usr;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.todays_food_list_row, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        myViewHolder.text1.setText(((FoodItem) itemsList.get(i).getItem()).getName());
+        myViewHolder.text2.setText(String.valueOf(((FoodDiaryItem) itemsList.get(i)).getPortion()) + " g");
+        double kCal = (((FoodItem) itemsList.get(i).getItem()).getEnergyKcal()) / 100 * (((FoodDiaryItem) itemsList.get(i)).getPortion());
+        myViewHolder.text3.setText(String.valueOf((int) kCal) + " kCal");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView text1, text2, text3;
+        ConstraintLayout mainLayout;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            text1 = itemView.findViewById(R.id.textTodayFoodListItem);
+            text2 = itemView.findViewById(R.id.textVievItemPortion);
+            text3 = itemView.findViewById(R.id.textViewPortionCal);
+            mainLayout = itemView.findViewById(R.id.mainTodayLayout);
+        }
+    }
+
+}
--- app/src/main/java/com/example/fatcarbon/UnitListAdapter.java
+++ app/src/main/java/com/example/fatcarbon/UnitListAdapter.java
@@ -2,6 +2,7 @@ package com.example.fatcarbon;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class UnitListAdapter extends RecyclerView.Adapter<UnitListAdapter.MyViewHolder>{
@@ -38,19 +41,35 @@ public class UnitListAdapter extends RecyclerView.Adapter<UnitListAdapter.MyView
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, @SuppressLint("RecyclerView") int i) {
         myViewHolder.text1.setText(item.getUnits().get(myViewHolder.getAdapterPosition())[0]);
         myViewHolder.text2.setText(item.getUnits().get(myViewHolder.getAdapterPosition())[1] + "g");
-        myViewHolder.mainLayout.setOnClickListener(new View.OnClickListener(){
+
+        myViewHolder.addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                user.getDiary().addEntry(new FoodDiaryItem(item,
-                        Double.parseDouble(item.getUnits().get(myViewHolder.getAdapterPosition())[1])));
+                Double foodPortion = Double.parseDouble(item.getUnits().get(myViewHolder.getAdapterPosition())[1]);
+                int foodPortionFactor;
+                if (!myViewHolder.textPortions.getText().toString().equals("")){
+                    foodPortionFactor = Integer.parseInt(myViewHolder.textPortions.getText().toString());
+                } else {
+                    foodPortionFactor = 1;
+                }
+                user.getDiary().addEntry(new FoodDiaryItem(item, foodPortion * foodPortionFactor));
+                Bundle args = new Bundle();
+                args.putSerializable("user", user);
                 Fragment fv = new FoodsViewFragment();
+                fv.setArguments(args);
                 FragmentManager manager = ((MainActivityLoggedIn) context).getSupportFragmentManager();
                 FragmentTransaction tranact = manager.beginTransaction();
                 tranact.replace(R.id.foodsFragmentLayout, fv);
                 tranact.commit();
-
             }
         });
+        //        myViewHolder.mainLayout.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+
+//
+//            }
+//        });
     }
 
     @Override
@@ -61,11 +80,15 @@ public class UnitListAdapter extends RecyclerView.Adapter<UnitListAdapter.MyView
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView text1, text2;
+        EditText textPortions;
+        Button addBtn;
         ConstraintLayout mainLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             text1 = itemView.findViewById(R.id.textFoodUnitItem);
             text2 = itemView.findViewById(R.id.textFoodUnitUnit);
+            textPortions = itemView.findViewById(R.id.editTextPortions);
+            addBtn = itemView.findViewById(R.id.buttonAddFoodItem);
             mainLayout = itemView.findViewById(R.id.mainUnitLayout);
         }
     }
