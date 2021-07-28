ad92b180776d3f2ca7f0b768400d51d92e260be6
sami <sami.sillanpaa@iki.fi>
2021-07-28 19:24:20 +0300

    Added weight target in weighting

--- app/src/main/java/com/example/fatcarbon/ui/weights/WeightsFragment.java
+++ app/src/main/java/com/example/fatcarbon/ui/weights/WeightsFragment.java
@@ -57,17 +57,28 @@ public class WeightsFragment extends Fragment {
         weightGraph.getLegendRenderer().setVisible(false);
         ArrayList<DiaryItem> weights = user.getDiary().getWeightEntries();
         DataPoint[] pointsArray = new DataPoint[weights.size()];
+        DataPoint[] targetArray = new DataPoint[2];
+        targetArray[0] = new DataPoint(weights.get(0).getDate(),
+                ((WeightDiaryItem) weights.get(0)).getWeightValue() * 0.9);
+        targetArray[1] = new DataPoint(weights.get(weights.size()-1).getDate(),
+                ((WeightDiaryItem) weights.get(0)).getWeightValue() * 0.9);
         for (int i = 0; i < weights.size(); i++) {
             pointsArray[i] = new DataPoint(weights.get(i).getDate(),
                     ((WeightDiaryItem) weights.get(i)).getWeightValue());
         }
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(pointsArray);
+        LineGraphSeries<DataPoint> seriesTarget = new LineGraphSeries<DataPoint>(targetArray);
         series.setTitle("weights");
         series.setColor(Color.BLUE);
         series.setDrawDataPoints(true);
         series.setDataPointsRadius(10);
         series.setThickness(8);
+        seriesTarget.setColor(Color.RED);
+        series.setDrawDataPoints(false);
+        series.setThickness(8);
         weightGraph.addSeries(series);
+        weightGraph.addSeries(seriesTarget);
+
         // set date label formatter
         weightGraph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
         weightGraph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
