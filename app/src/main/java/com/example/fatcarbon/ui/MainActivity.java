21f051f5244b784a2537f0b62feee70304b9db25
sami <sami.sillanpaa@iki.fi>
2021-07-30 21:09:39 +0300

    Toiminnallisuus  valmis.

--- app/src/main/java/com/example/fatcarbon/ui/MainActivity.java
+++ app/src/main/java/com/example/fatcarbon/ui/MainActivity.java
@@ -75,10 +75,10 @@ public class MainActivity extends AppCompatActivity {
             uname.setError(getString(R.string.pl_enter_uname));
             dataOK = false;
         }
-//        if (!pv.validatePassword(password.getText().toString())){
-//            password.setError(getString(R.string.pl_enter_password));
-//            dataOK = false;
-//        }
+        if (!pv.validatePassword(password.getText().toString())){
+            password.setError(getString(R.string.pl_enter_password));
+            dataOK = false;
+        }
         if (dataOK){
             PasswordHasher hasher = new PasswordHasher(password.getText().toString());
             User user = new User(uname.getText().toString(), hasher);
@@ -127,62 +127,6 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
 
     }
-
-
-//    public void login(View v) {
-//        EditText userName = findViewById(R.id.editTextUserName);
-//        EditText passWord = findViewById(R.id.editTextPasswd);
-//        PasswordValidator pv = new PasswordValidator();
-//        System.out.println("HERE");
-//        if (userName.length() == 0){
-//            userName.setError(getString(R.string.pl_enter_uname));
-//        }
-//        if (!pv.validatePassword(passWord.getText().toString())){
-//            passWord.setError(getString(R.string.pl_enter_password));
-//        }
-
-//        FoodItem fi = new FoodItem();
-//        DiaryItem di = new DiaryItem(fi);
-//        System.out.println(di.getItem().getClass());
-//        System.out.println(di.getItem() instanceof FoodItem);
-//        System.out.println(di.getDate());
-
-    /**
-     Shared prefs testing
-     */
-//        SharedPreferences sharedPreferences = getSharedPreferences("FatCarbSharedPref",MODE_PRIVATE);
-//        SharedPreferences.Editor myEdit = sharedPreferences.edit();
-//        myEdit.putStringSet("uname", new HashSet<String>(Arrays.asList("a", "b", "c")));
-//        myEdit.commit();
-//        SharedPreferences SH = getSharedPreferences("FatCarbSharedPref", MODE_APPEND);
-//        System.out.println(SH.getStringSet("uname", new HashSet<String>()).toString());
-
-    /**
-     Serialization tests
-     */
-
-//        UserDataReader rd = new UserDataReader(context);
-//        User user = (User) rd.readItem("Sama");
-//        System.out.println(user.validateUser("Sama", "Password124"));
-//        System.out.println(user.getPasswordHasher().validatePassword("Password124"));
-    /**
-     PWD Hasher testing
-     */
-//        PasswordHasher pw = new PasswordHasher("Password123");
-//        PasswordHasher pw1 = new PasswordHasher("Password124");
-//        User usr = new User("Sami", pw);
-//        User usr1 = new User("Sama", pw1);
-//        UserDataWriter dw = new UserDataWriter(context);
-//        dw.writeItem(usr);
-//        dw.writeItem(usr1);
-    /**
-     Fineli Api tests
-     */
-//        FineliApi api = new FineliApi();
-//        for (FoodItem item:api.parseFineliData("Kala")){
-//            System.out.println(item.getName().toString());
-//        }
-//    }
 
 
 }
