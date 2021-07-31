35d617c14d586f7bd009bc8a4eac263a0ad9173a
sami <sami.sillanpaa@iki.fi>
2021-07-31 16:29:35 +0300

    Added comments

--- app/src/main/java/com/example/fatcarbon/app/ActivityDiaryItem.java
+++ app/src/main/java/com/example/fatcarbon/app/ActivityDiaryItem.java
@@ -1,113 +1,64 @@
 package com.example.fatcarbon.app;
 
+/**************************************
+ LUT Olio-ohjelmointi Harjoitustyö
+ @author Sami Sillanpää
+ @copyright Sami Sillanpää 2021
+ @licence GNU GPL3.0
+ **************************************/
 
 /**
- * Class ActivityItem
+ * Class ActivityItem, hold data for a single activity
  */
 public class ActivityDiaryItem extends DiaryItem{
 
-    //
-    // Fields
-    //
 
     private String sport;
     private Long duration;
     private double met;
 
 
-    //
-    // Constructors
-    //
     public ActivityDiaryItem() {
     }
 
-    ;
-
-    //
-    // Methods
-    //
-
-
-    //
-    // Accessor methods
-    //
-
-    /**
-     * Set the value of sport
-     *
-     * @param newVar the new value of sport
-     */
     public void setSport(String newVar, int pos) {
         sport = newVar;
         met = calculateMetValue(pos);
     }
 
-    /**
-     * Get the value of sport
-     *
-     * @return the value of sport
-     */
+
     public String getSport() {
         return sport;
     }
 
-//    /**
-//     * Set the value of date
-//     *
-//     * @param newVar the new value of date
-//     */
-//    public void setDate(Date newVar) {
-//        date = newVar;
-//    }
-//
-//    /**
-//     * Get the value of date
-//     *
-//     * @return the value of date
-//     */
-//    public Date getDate() {
-//        return date;
-//    }
-
-    /**
-     * Set the value of duration
-     *
-     * @param newVar the new value of duration
-     */
     public void setDuration(Long newVar) {
         duration = newVar;
     }
 
-    /**
-     * Get the value of duration
-     *
-     * @return the value of duration
-     */
+
     public Long getDuration() {
         return duration;
     }
 
     public String getDurationString(){
+        /**
+         * Method returns a string representation of the activity duration
+         */
         double durInh =  duration.doubleValue() / 3600000; //duration in h
         return String.valueOf((int) durInh)+ "h " + (int)((durInh - (int) durInh) * 60) +"min" ;
     }
 
 
-    /**
-     * Get the value of calories
-     *
-     * @return the value of calories
-     */
     public double getCalories(double weight) {
         return met * weight * duration / 3600000;
     }
 
 
-
-    //
-    // Other methods
-    //
     private int calculateMetValue(int pos) {
+        /**
+         * Helper method to calculate the met value
+         * mets[] -> array represents mets values for each item in @string/mets
+         */
         int[] mets = {2, 3, 5, 6, 6, 6, 7, 9, 9, 9, 10, 12, 14, 15, 16, 17};
         return mets[pos];
 
