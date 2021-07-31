102c5ec36fd09233a5d64d1bcf7bc7ab2458b802
sami <sami.sillanpaa@iki.fi>
2021-07-31 14:45:00 +0300

    Added comments

--- app/src/main/java/com/example/fatcarbon/ui/foods/TodaysFoodListAdapter.java
+++ app/src/main/java/com/example/fatcarbon/ui/foods/TodaysFoodListAdapter.java
@@ -1,5 +1,13 @@
 package com.example.fatcarbon.ui.foods;
 
+/**************************************
+ LUT Olio-ohjelmointi Harjoitustyö
+ @author Sami Sillanpää
+ @copyright Sami Sillanpää 2021
+ @licence GNU GPL3.0
+ **************************************/
+
+
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
@@ -16,6 +24,10 @@ import com.example.fatcarbon.app.FoodItem;
 import com.example.fatcarbon.app.User;
 
 import java.util.ArrayList;
+
+/**
+ * RecyclerView adapter for the consumed food item list
+ */
 
 public class TodaysFoodListAdapter extends RecyclerView.Adapter<TodaysFoodListAdapter.MyViewHolder>{
 
@@ -39,8 +51,14 @@ public class TodaysFoodListAdapter extends RecyclerView.Adapter<TodaysFoodListAd
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+        // Set the item description text
         myViewHolder.text1.setText(((FoodItem) itemsList.get(i).getItem()).getName());
+
+        // Set the text for anoumt comsumed (in grams)
         myViewHolder.text2.setText(String.valueOf(((FoodDiaryItem) itemsList.get(i)).getAmount()) + " g");
+
+        // set th text value for calories consumed
         double kCal = (((FoodItem) itemsList.get(i).getItem()).getEnergyKcal()) / 100 * (((FoodDiaryItem) itemsList.get(i)).getAmount());
         myViewHolder.text3.setText(String.valueOf((int) kCal) + " kCal");
 
