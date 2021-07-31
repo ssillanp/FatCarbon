49103603a85f7562e46f10e22d7dae6ff3b147e8
sami <sami.sillanpaa@iki.fi>
2021-07-31 14:27:33 +0300

    Added comments

--- app/src/main/java/com/example/fatcarbon/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/fatcarbon/ui/home/HomeFragment.java
@@ -1,6 +1,11 @@
 package com.example.fatcarbon.ui.home;
+/**************************************
+ LUT Olio-ohjelmointi Harjoitustyö
+ @author Sami Sillanpää
+ @copyright Sami Sillanpää 2021
+ @licence GNU GPL3.0
+ **************************************/
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -18,18 +23,22 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * Home view fragment, days summary
+ */
 public class HomeFragment extends Fragment {
 
-//    private HomeViewModel homeViewModel;
 
 
-    @SuppressLint("SetTextI18n")
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        //retreive user from intent
         Intent intent = getActivity().getIntent();
         User user = (User) intent.getSerializableExtra("user");
+
+        // set the welcoming text and date
         TextView test = root.findViewById(R.id.text_home);
         test.setText("Welcome " + user.getUsername());
         TextView date = root.findViewById(R.id.text_home_date);
@@ -37,18 +46,28 @@ public class HomeFragment extends Fragment {
         SimpleDateFormat sdf = new SimpleDateFormat("dd.M.yyyy", new Locale("fi_FI"));
         System.out.println(sdf.format(now));
         date.setText(sdf.format(now));
+
+
         TextView calInTd = root.findViewById(R.id.textViewCalEatenKcal);
         TextView calInTdPct = root.findViewById(R.id.textViewCalEatenPrcent);
         TextView calBrntTd = root.findViewById(R.id.textViewCalBurntKcal);
         TextView wghtTd = root.findViewById(R.id.textViewCurrentWeight);
         TextView wghtToTrgt = root.findViewById(R.id.textViewWeightToTarget);
+
+        // create decimal formats for user friendly viewing
         DecimalFormat df = new DecimalFormat("#0");
         DecimalFormat dfp = new DecimalFormat("#0.0");
+
+        // set the display value for today calorie intake
         calInTd.setText(String.valueOf(df.format(user.getDiary().getDailyCalIntake(now)) + " kCal"));
+        // set the dusplay value for calorie intake precentage of daily recommended calories
         calInTdPct.setText(String.valueOf(dfp.format(user.getDiary().getDailyCalIntake(now)
                 / user.calculateBaseCalories() * 100)) + "%");
+        // set the display text for calories burnt in days activities
         calBrntTd.setText(String.valueOf(df.format(user.getDiary().getDailyEnergyBurnt(now, user.getCurrentWeight()))) + " kCal");
+        // set the display text for current weight
         wghtTd.setText(String.valueOf(user.getCurrentWeight()) + " kg");
+        // set the display text for deviation from weight target
         wghtToTrgt.setText(String.valueOf((user.getCurrentWeight()) -user.getTargetWeight()) + " kg");
 
         return root;
--- app/src/main/java/com/example/fatcarbon/ui/weights/WeightsFragment.java
+++ app/src/main/java/com/example/fatcarbon/ui/weights/WeightsFragment.java
@@ -1,5 +1,13 @@
 package com.example.fatcarbon.ui.weights;
 
+/**************************************
+ LUT Olio-ohjelmointi Harjoitustyö
+ @author Sami Sillanpää
+ @copyright Sami Sillanpää 2021
+ @licence GNU GPL3.0
+ **************************************/
+
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -22,6 +30,10 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 
+/**
+ * Fragment for weight graph and adding weightings
+ */
+
 public class WeightsFragment extends Fragment {
 
 
@@ -29,43 +41,56 @@ public class WeightsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_weights, container, false);
+        // retreive user from intent
         Intent intent = getActivity().getIntent();
         User user = (User) intent.getSerializableExtra("user");
         Button weightButton = root.findViewById(R.id.buttonAddWeighting);
         EditText weightValue = root.findViewById(R.id.editTextWeightVal);
         weightButton.setOnClickListener(new View.OnClickListener() {
             @Override
+            // Add a new weighting
             public void onClick(View v) {
                 WeightDiaryItem wdi = new WeightDiaryItem();
                 wdi.setWeightValue(Double.parseDouble(weightValue.getText().toString()));
                 wdi.setDateNow();
+                // add the value in diary
                 user.getDiary().addEntry(wdi);
+
+                // save user data in storage
                 UserDataWriter udw = new UserDataWriter(getActivity());
                 udw.writeItem(user);
                 updateGraph(user, root);
 
             }
         });
+        //update the weight graph
         updateGraph(user, root);
         return root;
     }
 
+    // Update the weight graph
     public void updateGraph(User user, View root) {
 
         GraphView weightGraph = root.findViewById(R.id.weight_graph);
         weightGraph.removeAllSeries();
         weightGraph.getLegendRenderer().setVisible(false);
+        // get all weight entries from user
         ArrayList<DiaryItem> weights = user.getDiary().getWeightEntries();
-        DataPoint[] pointsArray = new DataPoint[weights.size()];
-        DataPoint[] targetArray = new DataPoint[weights.size()];
+
+        // datapoint arrays for graph
+        DataPoint[] pointsArray = new DataPoint[weights.size()]; //actual weight graph
+        DataPoint[] targetArray = new DataPoint[weights.size()]; // target weight limit line
+
         for (int i = 0; i < weights.size(); i++) {
             pointsArray[i] = new DataPoint(weights.get(i).getDate(),
                     ((WeightDiaryItem) weights.get(i)).getWeightValue());
             targetArray[i] = new DataPoint(weights.get(0).getDate(),
                     user.getTargetWeight());
         }
+        // create data series for the graph
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(pointsArray);
         LineGraphSeries<DataPoint> seriesTarget = new LineGraphSeries<DataPoint>(targetArray);
+        // set series attributes (looks)
         series.setTitle("weights");
         series.setColor(Color.BLUE);
         series.setDrawDataPoints(true);
@@ -74,6 +99,8 @@ public class WeightsFragment extends Fragment {
         seriesTarget.setColor(Color.RED);
         seriesTarget.setDrawDataPoints(false);
         seriesTarget.setThickness(8);
+
+        // add the series in the graph
         weightGraph.addSeries(series);
         weightGraph.addSeries(seriesTarget);
 
@@ -81,16 +108,16 @@ public class WeightsFragment extends Fragment {
         weightGraph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
         weightGraph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
 
-// set manual x bounds to have nice steps
-        weightGraph.getViewport().setMinX(weights.get(0).getDate().getTime() - (86400000 / 2));
-        weightGraph.getViewport().setMaxX(weights.get(weights.size() - 1).getDate().getTime() + (86400000 / 2));
+        // set manual x and y bounds to have nice steps
+        weightGraph.getViewport().setMinX(weights.get(0).getDate().getTime() - 43200000); //43200000 -> 1/2 day in ms.
+        weightGraph.getViewport().setMaxX(weights.get(weights.size() - 1).getDate().getTime() + 43200000);
         weightGraph.getViewport().setMinY(((WeightDiaryItem) weights.get(0)).getWeightValue() * 0.4);
         weightGraph.getViewport().setMaxY(((WeightDiaryItem) weights.get(0)).getWeightValue() * 1.6);
         weightGraph.getViewport().setXAxisBoundsManual(true);
         weightGraph.getViewport().setYAxisBoundsManual(true);
 
-// as we use dates as labels, the human rounding to nice readable numbers
-// is not necessary
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
         weightGraph.getGridLabelRenderer().setHumanRounding(false);
 
     }
