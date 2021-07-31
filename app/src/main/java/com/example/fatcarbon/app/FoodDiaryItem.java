8c7cf1b829be2c5a8a25e9dc1549b1744436e72e
sami <sami.sillanpaa@iki.fi>
2021-07-31 19:48:14 +0300

    added comments

--- app/src/main/java/com/example/fatcarbon/app/FoodDiaryItem.java
+++ app/src/main/java/com/example/fatcarbon/app/FoodDiaryItem.java
@@ -1,5 +1,16 @@
 package com.example.fatcarbon.app;
 
+/**************************************
+ LUT Olio-ohjelmointi Harjoitustyö
+ @author Sami Sillanpää
+ @copyright Sami Sillanpää 2021
+ @licence GNU GPL3.0
+ **************************************/
+
+
+/**
+ * Class for food diary items
+ */
 public class FoodDiaryItem extends DiaryItem {
 
 
